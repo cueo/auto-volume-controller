4001e1ce2dd603fae060ae984b7868ac9d1e4cf7
cueo <mohit.ritanil@gmail.com>
2017-10-25 09:58:27 +0530

    Updated test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import applescript
 
-val = 20
+val = 60
 # applescript.AppleScript('set volume output volume ' + str(val)).run()
 x = applescript.AppleScript('get volume settings').run()
 print x[applescript.AEType('ouvl')]
